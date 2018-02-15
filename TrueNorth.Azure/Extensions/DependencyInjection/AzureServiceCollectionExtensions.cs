a3a2aa55545d2c7a922be22a733292d21d83454e
Unknown <cwaters@truenorthit.co.uk>
2018-02-15 14:39:20 +0000

    test checkin

--- TrueNorth.Azure/Extensions/DependencyInjection/AzureServiceCollectionExtensions.cs
+++ TrueNorth.Azure/Extensions/DependencyInjection/AzureServiceCollectionExtensions.cs
@@ -30,5 +30,37 @@ namespace TrueNorth.Extensions.DependencyInjection
                 }
             );
         }
+
+        public static void AddTableStorage(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<CloudTable>(s =>
+            {
+                var tableStorageOptions = s.GetService<IOptions<TableStorageOptions>>().Value;
+
+                var azureLogConnection = tableStorageOptions.AzureTableStorageConnection;
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(azureLogConnection);
+                CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+                CloudTable cloudtable = tableClient.GetTableReference("FastReserveLogs");
+
+                var task = Task.Run(async () => await cloudtable.CreateIfNotExistsAsync());
+                task.Wait();
+
+                return cloudtable;
+
+            });
+
+            //serviceCollection.AddSingleton<ILoggerFactory>(s =>
+            //{
+            //    var tableStorageOptions = s.GetService<IOptions<TableStorageOptions>>().Value;
+            //    var logWriter = s.GetService<BufferedLogWriter<LogEntryForWeb>>();
+            //    var loggerFactory = new LoggerFactory();
+
+            //    var minLogLevel = TrueNorth.Azure.LoggerExtensions.ParseLogLevel(tableStorageOptions.MinLevel);
+
+            //    loggerFactory.AddProvider(new AzureTableStorageLoggerProvider(logWriter, minLogLevel));
+            //    return loggerFactory;
+            //});
+        }
+
     }
 }
