ed9e0861514823e806b35438e7604e3e5780c86f
Unknown <cwaters@truenorthit.co.uk>
2018-04-28 11:19:30 +0100

    Task extension

---
+++ TrueNorth.Azure/Extensions/TaskExtensions/TaskExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueNorth.Azure.Extensions.TaskExtensions
+{
+    public static class TaskExtensions
+    {
+        public static async void Forget(this Task task, params Type[] acceptableExceptions)
+        {
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                // TODO: consider whether derived types are also acceptable.
+                if (!acceptableExceptions.Contains(ex.GetType()))
+                    throw;
+            }
+        }
+    }
+}
