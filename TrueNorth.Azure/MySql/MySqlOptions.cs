ba45e114b2d5c6216eafd273707d1e8ec5038f02
Unknown <cwaters@truenorthit.co.uk>
2018-05-08 15:37:41 +0100

    added mysql option

---
+++ TrueNorth.Azure/MySql/MySqlOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrueNorth.Azure.MySql
+{
+    public class MySqlOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}
