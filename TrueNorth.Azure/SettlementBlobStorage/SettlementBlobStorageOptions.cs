0a9c315c5ad91420ae1ff04c4d287f21e216dabb
chriswaters78 <cwaters@truenorthit.co.uk>
2023-09-27 14:33:10 +0100

    Feature/jdp/#6156 settlements (#2)
    * SettlementBlobStorageOptions
    
    * upgrade packages
    
    ---------
    
    Co-authored-by: Joe Pitts <JPitts@TrueNorthIT.co.uk>

---
+++ TrueNorth.Azure/SettlementBlobStorage/SettlementBlobStorageOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrueNorth.Azure
+{
+    public class SettlementBlobStorageOptions
+    {
+        public string StorageConnectionString { get; set; }
+        public string ContainerName { get; set; }
+
+    }
+}
