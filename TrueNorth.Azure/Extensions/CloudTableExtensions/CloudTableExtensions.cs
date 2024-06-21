e3cb16489316dbcf76743c3fff8a68261cae3ef8
Christian <cwaters@truenorthit.co.uk>
2024-06-21 11:33:20 +0100

    half done - https://learn.microsoft.com/en-gb/azure/cosmos-db/nosql/migrate-dotnet-v3

--- TrueNorth.Azure/Extensions/CloudTableExtensions/CloudTableExtensions.cs
+++ TrueNorth.Azure/Extensions/CloudTableExtensions/CloudTableExtensions.cs
@@ -11,17 +11,18 @@ namespace TrueNorth.Azure.Extensions.CloudTableEx
 {
     public static class CloudTableExtensions
     {
-        public static async Task<IList<T>> ExecuteQueryAsync<T>(this TableClient tableClient, string tableName, Expression<Func<T, bool>> filter, List<string> columns, CancellationToken ct = default(CancellationToken), Action<IList<T>> onProgress = null) where T : class, ITableEntity, new()        {
+        public static async Task<IList<T>> ExecuteQueryAsync<T>(this TableClient tableClient, Expression<Func<T, bool>> filter, Action<IReadOnlyList<T>> onProgress = null) where T : class, ITableEntity, new()        {
             
             var items = new List<T>();
 
             string continuationToken = null;
 
-            var pageable = tableClient.QueryAsync<T>(filter: filter, maxPerPage: 1000, select: columns, cancellationToken: ct);
+            var pageable = tableClient.QueryAsync<T>(filter: filter, maxPerPage: 1000);
 
             await using (IAsyncEnumerator<Page<T>> enumerator = pageable.AsPages(continuationToken).GetAsyncEnumerator())
             {
                 await enumerator.MoveNextAsync();
+                onProgress(enumerator.Current.Values);
                 foreach (var item in enumerator.Current.Values)
                 {
                     items.Add(item);
